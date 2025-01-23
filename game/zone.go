9dbf9aff4ccbc7a621088bdf400cd1ad7298264e
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-23 17:01:54 -0500

    Added comments

--- game/zone.go
+++ game/zone.go
@@ -4,6 +4,9 @@ import (
 	"github.com/stevezaluk/mtgjson-models/user"
 )
 
+/*
+Zone - Represents a Zone (or container) where cards can be placed within the game
+*/
 type Zone struct {
 	ZoneId string
 
@@ -15,6 +18,10 @@ type Zone struct {
 	IsOrdered bool
 }
 
+/*
+NewZone - A constructor provided for creating a new Zone. An owner and isShared can not be declared as once,
+as if a Zone is shared then it cannot have an owner
+*/
 func NewZone(zoneId string, owner *user.User, isPublic bool, isShared bool, isOrdered bool) *Zone {
 	if owner != nil && isShared {
 		return nil
