934a4c11c1b4a49aa0f420c0f19b7f443ef8f771
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-23 14:31:15 -0500

    Defined the basic structure of the game zone

--- game/zone.go
+++ game/zone.go
@@ -1,1 +1,16 @@
 package game
+
+import (
+	"github.com/stevezaluk/mtgjson-models/user"
+)
+
+type Zone struct {
+	ZoneId string
+	Owner  *user.User
+
+	// cards here
+
+	IsPublic  bool
+	IsShared  bool
+	IsOrdered bool
+}
