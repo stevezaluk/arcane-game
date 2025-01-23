e429b16d1bce1779d99a632b7400fe31a97fe120
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-23 17:25:11 -0500

    Defined the structure for the DeckObject

--- game/deck.go
+++ game/deck.go
@@ -1,2 +1,15 @@
 package game
 
+import "github.com/stevezaluk/mtgjson-models/deck"
+
+/*
+DeckObject - Represents the deck zone that the player interacts with through the game
+*/
+type DeckObject struct {
+	Metadata   *deck.Deck
+	Owner      *Player
+	Controller *Player
+	Zone       *Zone
+
+	IsTopCardRevealed bool
+}
