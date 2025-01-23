f41dbb946d3afcb2761d574f9e90091cd4a56be1
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-23 17:00:32 -0500

    Provided a constructor for the Zone struct. Removed the Owner field from the Zone for now as this is going to be a pointer to the Player struct instead of the User struct

--- game/zone.go
+++ game/zone.go
@@ -6,11 +6,24 @@ import (
 
 type Zone struct {
 	ZoneId string
-	Owner  *user.User
 
+	// Owner (*Player) here
 	// cards here
 
 	IsPublic  bool
 	IsShared  bool
 	IsOrdered bool
+}
+
+func NewZone(zoneId string, owner *user.User, isPublic bool, isShared bool, isOrdered bool) *Zone {
+	if owner != nil && isShared {
+		return nil
+	}
+
+	return &Zone{
+		ZoneId:    zoneId,
+		IsPublic:  isPublic,
+		IsShared:  isShared,
+		IsOrdered: isOrdered,
+	}
 }
