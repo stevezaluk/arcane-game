a52e309d5778b262eefb8a319bb2059ad3ffc909
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-23 17:13:07 -0500

    Defined the structure of a player

--- game/player.go
+++ game/player.go
@@ -1,1 +1,28 @@
 package game
+
+import (
+	"github.com/stevezaluk/mtgjson-models/user"
+	"net"
+)
+
+/*
+Player - Represents a player interacting with the game
+*/
+type Player struct {
+	User *user.User
+	Conn net.Conn
+
+	Graveyard *Zone
+	Hand      *Zone
+
+	// mana pool
+
+	LifeTotal          int
+	CommanderDamage    int
+	PoisonCounters     int
+	EnergyCounters     int
+	ExperienceCounters int
+
+	IsMonarch   bool
+	IsGameOwner bool
+}
