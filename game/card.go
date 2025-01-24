b2dddefbf6d324de3c332bf810ca7cbd3e93c63b
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-24 11:35:09 -0500

    Defined the basic CardObject structure

--- game/card.go
+++ game/card.go
@@ -1,1 +1,19 @@
 package game
+
+import "github.com/stevezaluk/mtgjson-models/card"
+
+/*
+CardObject - Represents the card played or generated for the game. This needs to differ
+from the protobuf models as there are additional values that need to be tracked like
+ownership, its parent zone, and the state of the card
+*/
+type CardObject struct {
+	Metadata   *card.CardSet
+	Owner      *Player
+	Controller *Player
+	ParentZone *Zone
+
+	IsTapped          bool
+	IsFaceDown        bool
+	WasPlayedThisTurn bool
+}
