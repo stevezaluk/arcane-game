2dcbff6c3361a08a832067e70dd9402e623f3eeb
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-24 12:28:23 -0500

    Defined the structure of a MTG game as a whole

--- game/game.go
+++ game/game.go
@@ -1,3 +1,21 @@
 package game
 
+import (
+	"github.com/stevezaluk/mtgjson-sdk-client/api"
+)
 
+/*
+Game - A representation of a single MTG Game. Process's game commands sent from the client
+*/
+type Game struct {
+	Name     string
+	GameMode string
+
+	Players   []*Player
+	GameOwner *Player
+
+	Battlefield *Zone
+	Exile       *Zone
+	Command     *Zone
+	API         *api.MtgjsonApi
+}
