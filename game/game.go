dacfc4210a7bca3bd6be2c55c41570541f238b61
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-24 22:02:50 -0500

    Implemented a constructor for the Game object. This initializes zones related to the Game

--- game/game.go
+++ game/game.go
@@ -4,6 +4,12 @@ import (
 	"github.com/stevezaluk/mtgjson-sdk-client/api"
 )
 
+const (
+	CommanderGameMode = "gamemode:commander"
+	ModernGameMode    = "gamemode:modern"
+	StandardGameMode  = "gamemode:standard"
+)
+
 /*
 Game - A representation of a single MTG Game. Process's game commands sent from the client
 */
@@ -18,4 +24,26 @@ type Game struct {
 	Exile       *Zone
 	Command     *Zone
 	API         *api.MtgjsonApi
+}
+
+/*
+NewGame - Initialize the zones of a new Game and return a pointer to it
+*/
+func NewGame(name string, gameMode string) *Game {
+	battlefield := NewZone(BattlefieldZoneId, nil, true, true, false)
+	exile := NewZone(ExileZoneId, nil, true, true, false)
+
+	var command *Zone
+	if gameMode == CommanderGameMode {
+		command = NewZone(CommanderZoneId, nil, true, true, false)
+	}
+
+	return &Game{
+		Name:        name,
+		GameMode:    gameMode,
+		Battlefield: battlefield,
+		Exile:       exile,
+		Command:     command,
+		API:         api.New(),
+	}
 }
