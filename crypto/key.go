cbdcf6782e4796a715d51fb831216a7291fe1157
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-27 13:53:57 -0500

    Defined KeyPair structure to store a RSA key pair

--- crypto/key.go
+++ crypto/key.go
@@ -1,3 +1,35 @@
 package crypto
 
+import (
+	"crypto/rand"
+	"crypto/rsa"
+)
 
+/*
+KeyPair - Represents a private/public 4096-bit RSA key pair
+*/
+type KeyPair struct {
+	privateKey *rsa.PrivateKey
+	PublicKey  rsa.PublicKey
+}
+
+/*
+NewKeyPair - A constructor for the *KeyPair object. Generates and validates a RSA key and
+stores it within the KeyPair structure
+*/
+func NewKeyPair() (*KeyPair, error) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return nil, err
+	}
+
+	err = privateKey.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return &KeyPair{
+		privateKey: privateKey,
+		PublicKey:  privateKey.PublicKey,
+	}, nil
+}
