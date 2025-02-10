c0184e5c1856c250cffcde15b883bc1e2de0f519
Steven A. Zaluk <stevezaluk@gmail.com>
2025-02-10 10:50:14 -0500

    Defined the structure for the encryption handler

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -1,3 +1,23 @@
 package crypto
 
+/*
+EncryptionHandler - Contains logic for exchanging keys between the server and client, and
+holds logic for sending encrypted messages
+*/
+type EncryptionHandler struct {
+	serverKey *KeyPair
+	clientKey *KeyPair
+}
 
+/*
+HandlerFromServerKey - Creates a new encryption handler from an existing server key. This generates
+a fresh key pair that can be used with the client (specifically within the context of a player)
+*/
+func HandlerFromServerKey(serverKey *KeyPair) (*EncryptionHandler, error) {
+	clientKey, err := NewKeyPair()
+	if err != nil {
+		return nil, err
+	}
+
+	return &EncryptionHandler{serverKey: serverKey, clientKey: clientKey}, nil
+}
