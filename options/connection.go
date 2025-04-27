e4552ccf323ef510e95beb65f6c5bf796d5556d7
Steven A. Zaluk <stevezaluk@gmail.com>
2025-04-27 14:41:29 -0400

    Implemented constructors for the ConnectionOptions struct

--- options/connection.go
+++ options/connection.go
@@ -1,6 +1,9 @@
 package server
 
-import "time"
+import (
+	"github.com/spf13/viper"
+	"time"
+)
 
 /*
 ConnectionOptions - A structure for tracking parameterized options to use relating to client and server connections
@@ -10,10 +13,10 @@ type ConnectionOptions struct {
 	MaxConnectionCount uint32
 
 	// ClientTimeout - The number of seconds the server should wait for a client to complete Key Exchange (assuming it is enabled)
-	ClientTimeout *time.Duration
+	ClientTimeout time.Duration
 
 	// WaitConnectionsTimeout - The number of seconds the server should wait to accept new clients before closing new connections
-	WaitConnectionsTimeout *time.Duration
+	WaitConnectionsTimeout time.Duration
 
 	// EnforceACLs - If set to true, then enforce the lists defined in ConnectionOptions.Whitelist and ConnectionOptions.Blacklist
 	EnforceACLs bool
@@ -23,4 +26,25 @@ type ConnectionOptions struct {
 
 	// Blacklist - Explicitly block the IP Addresses defined in this slice
 	Blacklist []string
+}
+
+/*
+Connection - Returns an empty ConnectionOptions struct
+*/
+func Connection() *ConnectionOptions {
+	return &ConnectionOptions{}
+}
+
+/*
+FromConfig - Fills the ConnectionOptions struct with values pulled from Viper
+*/
+func FromConfig() *ConnectionOptions {
+	return &ConnectionOptions{
+		MaxConnectionCount:     viper.GetUint32("server.options.max_connections"),
+		ClientTimeout:          viper.GetDuration("server.options.client_timeout"),
+		WaitConnectionsTimeout: viper.GetDuration("server.options.wait_connections_timeout"),
+		EnforceACLs:            viper.GetBool("server.options.enforce_acls"),
+		Whitelist:              viper.GetStringSlice("server.options.whitelist"),
+		Blacklist:              viper.GetStringSlice("server.options.blacklist"),
+	}
 }
