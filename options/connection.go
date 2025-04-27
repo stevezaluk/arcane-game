a9f1c2a346685388db46a059a9d1627f8c3b42f0
Steven A. Zaluk <stevezaluk@gmail.com>
2025-04-27 15:09:22 -0400

    Defined functions for setting common connection options

--- options/connection.go
+++ options/connection.go
@@ -40,11 +40,101 @@ FromConfig - Fills the ConnectionOptions struct with values pulled from Viper
 */
 func FromConfig() *ConnectionOptions {
 	return &ConnectionOptions{
-		MaxConnectionCount:     viper.GetUint32("server.options.max_connections"),
-		ClientTimeout:          viper.GetDuration("server.options.client_timeout"),
-		WaitConnectionsTimeout: viper.GetDuration("server.options.wait_connections_timeout"),
-		EnforceACLs:            viper.GetBool("server.options.enforce_acls"),
-		Whitelist:              viper.GetStringSlice("server.options.whitelist"),
-		Blacklist:              viper.GetStringSlice("server.options.blacklist"),
+		MaxConnectionCount:     viper.GetUint32("server.max_connections"),
+		ClientTimeout:          viper.GetDuration("server.client_timeout"),
+		WaitConnectionsTimeout: viper.GetDuration("server.wait_connections_timeout"),
+		EnforceACLs:            viper.GetBool("server.enforce_acls"),
+		Whitelist:              viper.GetStringSlice("server.whitelist"),
+		Blacklist:              viper.GetStringSlice("server.blacklist"),
 	}
+}
+
+/*
+SetMaxConnectionCount - Allows you to define the maximum amount of successful connections the server
+should accept. If this is not set, then the default value of 4 is used. This includes the "Game Leader",
+colloquially called the "Host".
+
+An upper limit for this is not set, so there truly is not limit to what you can set this number to (aside from the
+32-bit unsigned integer limit). I recommend setting this to 6 connections (or under) as feasibly there is no way to
+play a game of Magic: The Gathering with more than 6 people.
+*/
+func (opts *ConnectionOptions) SetMaxConnectionCount(max uint32) *ConnectionOptions {
+	opts.MaxConnectionCount = max
+
+	return opts
+}
+
+/*
+SetClientTimeout - Allows you to define the amount of time the client has to complete key exchange before
+there connection gets forcibly closed and removed from the server. Reconnections even after this has expired
+are still allowed.
+
+Timeout must be defined in seconds
+*/
+func (opts *ConnectionOptions) SetClientTimeout(timeout uint32) *ConnectionOptions {
+	opts.ClientTimeout = time.Second * time.Duration(timeout)
+
+	return opts
+}
+
+/*
+SetWaitConnectionsTimeout - Allows you to define the amount of time the server should wait to aggregate
+new connections before preventing new connections and starting game logic.
+
+Currently, there is no way for the Game Leader (an arbitrarily defined Player in control of the server) to force the
+Server to start, so the Server strictly abides by these timers and will not start until they expire. Be sure to properly
+configure these if the defaults are not suitable.
+
+If the servers successful connection count is zero after this timer fires, then the server is automatically closed
+and stopped as there is no need to process game logic with no active connections
+
+Timeout must be defined in seconds
+*/
+func (opts *ConnectionOptions) SetWaitConnectionsTimeout(timeout uint32) *ConnectionOptions {
+	opts.WaitConnectionsTimeout = time.Second * time.Duration(timeout)
+
+	return opts
+}
+
+/*
+SetEnforceACLs - Instructs the server to validate client IP Addresses present in ConnectionOptions.Whitelist
+and ConnectionOptions.Blacklist when accepting connections. Any client IP Addresses placed in these lists
+are ignored unless ConnectionOptions.EnforceACLs is set to true
+*/
+func (opts *ConnectionOptions) SetEnforceACLs() *ConnectionOptions {
+	opts.EnforceACLs = true
+
+	return opts
+}
+
+/*
+SetWhitelist - Set the server whitelist to the value defined in 'acl'. When a new client
+attempts to connect to the server, its reported IP Address is checked to ensure that it
+exists in this list. If it does not, then its connection is closed.
+
+This validation happens before key exchange, to ensure that sensitive information (like
+session keys) only get sent to trusted clients.
+
+If a client IP Address is improperly formatted (ex: 11.11.11.111111111) then it is ignored
+*/
+func (opts *ConnectionOptions) SetWhitelist(acl []string) *ConnectionOptions {
+	opts.Whitelist = acl
+
+	return opts
+}
+
+/*
+SetBlacklist - Set the server blacklist to the value defined in 'acl'. When a new client
+attempts to connect to the server, its reported IP Address is checked to ensure that it
+does not exist in this list. If it does, then its connection is closed.
+
+This validation happens before key exchange, to ensure that sensitive information (like
+session keys) only get sent to trusted clients
+
+If a client IP Address is improperly formatted (ex: 11.11.11.111111111) then it is ignored
+*/
+func (opts *ConnectionOptions) SetBlacklist(acl []string) *ConnectionOptions {
+	opts.Blacklist = acl
+
+	return opts
 }
