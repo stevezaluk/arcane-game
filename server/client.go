0e7b95bcf264565caa2ebfbf5b17286f41a2f0d7
Steven A. Zaluk <stevezaluk@gmail.com>
2025-04-27 16:30:52 -0400

    Defined a basic structure and constructor for the client

--- server/client.go
+++ server/client.go
@@ -1,3 +1,32 @@
 package server
 
+import (
+	"github.com/stevezaluk/arcane-game/crypto"
+	"net"
+)
 
+/*
+Client - A representation of a client connecting to the server. Processes Key Exchange between
+the client and the server and holds connection information for the client
+*/
+type Client struct {
+	// IPAddress - The remote IP Address of the client connection
+	IPAddress string
+
+	// Conn - The connection structure used for sending and receiving messages from the client
+	conn net.Conn
+
+	// cryptoHandler - Provides logic for generating encryption keys
+	cryptoHandler *crypto.EncryptionHandler
+}
+
+/*
+NewClient - A constructor for the Client structure. Creates new Client structure
+and returns a pointer to it
+*/
+func NewClient(conn net.Conn) *Client {
+	return &Client{
+		IPAddress: conn.RemoteAddr().String(),
+		conn:      conn,
+	}
+}
