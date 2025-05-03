807988c8ef4a9cb487d8430a7e26063bcfe4548c
Steven A. Zaluk <stevezaluk@gmail.com>
2025-05-03 18:30:55 -0400

    Defined basic constructors for CryptoOptions

--- options/crypto.go
+++ options/crypto.go
@@ -1,5 +1,7 @@
 package options
 
+import "github.com/spf13/viper"
+
 const (
 	// KEXRSA - A constant for defining the RSA Algorithm for Key Exchange
 	KEXRSA string = "RSA"
@@ -20,4 +22,27 @@ type CryptoOptions struct {
 
 	// KeySize - Defines the size in bits that RSA key's will be generated in. Cannot be lower than 2048
 	KeySize uint32
+}
+
+/*
+Crypto - Returns a pointer to a CryptoOptions struct, filled with the recommended options
+*/
+func Crypto() *CryptoOptions {
+	return &CryptoOptions{
+		ValidateKeys:        true,
+		EncryptionAlgorithm: KEXRSA,
+		KeySize:             KEYSIZE4096,
+	}
+}
+
+/*
+FromConfig - Fills the CryptoOptions struct with values pulled from Viper. Overwrites all pre-existing
+values
+*/
+func (crypto *CryptoOptions) FromConfig() *CryptoOptions {
+	return &CryptoOptions{
+		ValidateKeys:        viper.GetBool("kex.validate_keys"),
+		EncryptionAlgorithm: KEXRSA, // this is always set to RSA, as others are not supported at the moment
+		KeySize:             viper.GetUint32("kex.key_size"),
+	}
 }
