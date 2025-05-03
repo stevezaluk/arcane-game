5d82b8e9bd5fbbce33adeac8057c7df29ee5700f
Steven A. Zaluk <stevezaluk@gmail.com>
2025-05-03 18:39:08 -0400

    Defined setter values for the CryptoOptions struct

--- options/crypto.go
+++ options/crypto.go
@@ -39,10 +39,48 @@ func Crypto() *CryptoOptions {
 FromConfig - Fills the CryptoOptions struct with values pulled from Viper. Overwrites all pre-existing
 values
 */
-func (crypto *CryptoOptions) FromConfig() *CryptoOptions {
+func (opts *CryptoOptions) FromConfig() *CryptoOptions {
 	return &CryptoOptions{
 		ValidateKeys:        viper.GetBool("kex.validate_keys"),
 		EncryptionAlgorithm: KEXRSA, // this is always set to RSA, as others are not supported at the moment
 		KeySize:             viper.GetUint32("kex.key_size"),
 	}
+}
+
+/*
+SetValidateKeys - Allows you to control if the server should validate the keys that it receives. Generally, during Key
+Exchange when the server receives a client's key it will calculate the checksum of that key and expect a response from
+the client with its calculated value of the key. If the values match, then the key is accepted and the client moves
+to the next phase of Key Exchange. If this is set to true, then the server will blindly accept any key it is handed.
+
+Generally, this should stay on as it ensures that the key has not been modified during transmission. However, if the
+server.Server is running in an environment where it wants incredibly low latency and minimal chatter between client and
+server, then it can be disabled. No sensitive information is exposed between server/client communications aside from the
+player's email address
+*/
+func (opts *CryptoOptions) SetValidateKeys(validate bool) *CryptoOptions {
+	opts.ValidateKeys = validate
+
+	return opts
+}
+
+/*
+SetEncryptionAlgorithm - Allows you to define the symmetric encryption algorithm the server should use when starting
+Key Exchange. Currently, the 'algorithm' parameter is ignored and KEXRSA is set here as other algorithms are not
+supported.
+*/
+func (opts *CryptoOptions) SetEncryptionAlgorithm(algorithm string) *CryptoOptions {
+	opts.EncryptionAlgorithm = KEXRSA
+
+	return opts
+}
+
+/*
+SetKeySize - Allows you to define the size of the keys the server/client generates during Key Exchange. If this value
+is set any lower than 2048, it is reset back to 2048. Use either KEYSIZE2048 or KEYSIZE4096 to set these values.
+*/
+func (opts *CryptoOptions) SetKeySize(size uint32) *CryptoOptions {
+	opts.KeySize = size
+
+	return opts
 }
